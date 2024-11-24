1ab6f90e2f658099df664a8bc128b7ba79a273c2
Duy18ND <duy18namdinh@gmail.com>
2024-11-24 10:13:00 +0700

    Add files via upload

---
+++ sesion04-02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int age;
+	printf("moi ban nhap so nguyen bat ky  ",age);
+	 scanf("%d",&age);
+	 if(age % 2 == 0 ){
+	 	printf("%d la so chan",age); 
+	 } else{
+	 	printf("%d la so le",age); 
+	 } 
+	return 0; 
+} 
